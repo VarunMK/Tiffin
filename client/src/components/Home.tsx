5a664e87490f634bd3ed0880499ed01b7bd3207d
VarunMK <mkvarun2001@gmail.com>
2022-02-12 13:57:10 +0530

    Added file upload

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,34 +1,87 @@
-import React, { useRef, useState } from 'react'
-import { Box, Button,Input,Text,useQuery,useToast } from '@chakra-ui/react'
+import React, { useState } from 'react';
+import { createReadStream } from 'fs';
+import {
+    Box,
+    Button,
+    Flex,
+    Heading,
+    Input,
+    Text,
+    useQuery,
+    useToast,
+} from '@chakra-ui/react';
+import axios from 'axios';
 
+const Home = () => {
+    //use state for file data
+    const [fileData, setFileData] = useState<File>();
+    const toast = useToast();
+    const props = {
+        name: 'requirements',
+        action: '',
+    };
 
+    const createContainer = async (fileData:File) => {
+        const formData= new FormData();
+        formData.append('file',fileData,fileData.name)
 
-const Home=()=>{
-    const [s,setS]=useState<string>('Hi');
-    const toast=useToast();
-    const startContainer=async (s:string)=>{
-        try{
-            const data=await fetch('http://localhost:5000/startContainer',{
-                method:'POST',
-                headers:{'Content-Type':'application/JSON'},
-                body:JSON.stringify({'name':s})
+        try {
+            const {data}=await axios.post('http://localhost:5000/createContainer',formData);
+            console.log(data)
+            // const data = await fetch('http://localhost:5000/createContainer', {
+            //     method: 'POST',
+            //     headers:{'Content-Type':'multipart/form-data'},
+            //     body: formData,
+            // });
+            // const jsonData = await data.json();
+            // console.log(jsonData);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const startContainer = async () => {
+        try {
+            const data = await fetch('http://localhost:5000/startContainer', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/JSON' },
+                body: JSON.stringify({ name: 'hi' }),
             });
-            const jsonData=await data.json();
+            const jsonData = await data.json();
             console.log(jsonData);
+        } catch (err) {
+            console.log(err);
         }
-        catch(err){
-            console.log(err)
-        }
-    }
-    return(
-        <Box>
+    };
+    console.log(fileData);
+    return (
+        <Box width="50%" margin="0 auto">
+            <Box backgroundColor="gray.100" py="3" textAlign="center">
+                <Heading>Tiffin</Heading>
+            </Box>
             <Text>File Upload:</Text>
-            {/* <Input onChange={(e:any)=>{setS(e)}}>Text</Input> */}
-            <Button onClick={()=>{startContainer(s)}}>Build Container</Button>
-            <Button>Start Container</Button>
-            <Button>Stop Container</Button>            
+            <Flex direction="column">
+                <Input
+                    type="file"
+                    width="50%"
+                    onChange={(e: any) => {
+                        setFileData(e.target.files[0])
+                    }}
+                />
+                <Button
+                    mt="2"
+                    onClick={()=>{
+                        if(fileData!=undefined){
+                            createContainer(fileData)}}
+                        }
+                >
+                    Build Container
+                </Button>
+                <Button mt="2">Start Container</Button>
+                <Button mt="2">Stop Container</Button>
+            </Flex>
         </Box>
     );
-}
+};
 
 export default Home;
--- server/main.py
+++ server/main.py
@@ -1,10 +1,12 @@
 import json
+import os
 from flask import Flask, request,Response, jsonify
 from flask_cors import CORS,cross_origin
 app=Flask(__name__)
 CORS(app)
 
-
+UPLOAD_PATH='./uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_PATH
 @app.route('/')
 def health():
     return 'Server is running'
@@ -17,6 +19,22 @@ def resp():
     else:
         data=request.get_json()
         return jsonify(data)
+
+@app.route('/createContainer',methods=['POST','GET'])
+@cross_origin()
+def createContainer():
+    if(request.method=='GET'):
+        return jsonify({'message':'Success'})
+    else:
+        if('file' not in request.files):
+            return jsonify({'message':'No file part in the request','status':404})
+        file=request.files['file']
+        if file.filename=='':
+            return jsonify({'message':'No file selected for uploading','status':404})
+        if file:
+            print(os.path)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],file.filename))
+        return 'Success'
 
 def __init__(self):
     app.run(debug=True)
