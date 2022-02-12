97fe5c13f28557c51d82454e6b6611ef650cc2d7
Utkarsh Gupta <utkarsh348@gmail.com>
2022-02-12 18:58:29 +0530

    clean up

--- backend_driver/funcs.py
+++ backend_driver/funcs.py
@@ -6,8 +6,8 @@ import requests
 client = docker.from_env()
 def start(container):
     container = client.containers.start(container)
-def create_cont(reqs, image,pyver):
-    container = client.containers.run(image, detach = False, ports = {'3000/tcp':3000, '8080/tcp':8080})
+def create_cont(reqs,pyver):
+    container = client.containers.run('panzerox123/tiffin_contain', detach = False, ports = {'3000/tcp':3000, '8080/tcp':8080})
     URL = "localhost:8080/pyinstall/"+pyver
     r = requests.get(url = URL)
 def run(volume):
