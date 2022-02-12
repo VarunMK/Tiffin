504c1afe832f9408542a135d63e02b25d5c50664
Utkarsh Gupta <utkarsh348@gmail.com>
2022-02-12 18:57:09 +0530

    clean up

--- backend_driver/funcs.py
+++ backend_driver/funcs.py
@@ -1,12 +1,15 @@
 from pydoc import doc
 from time import sleep
 import docker
+import requests
 
 client = docker.from_env()
 def start(container):
     container = client.containers.start(container)
 def create_cont(reqs, image,pyver):
     container = client.containers.run(image, detach = False, ports = {'3000/tcp':3000, '8080/tcp':8080})
+    URL = "localhost:8080/pyinstall/"+pyver
+    r = requests.get(url = URL)
 def run(volume):
     #need to run from volume
     pass
@@ -15,4 +18,3 @@ def stop(container):
 def destroy(container):
     pass
 
-create_cont('reqs.txt', 'panzerox123/tiffin_contain', 3.9)
