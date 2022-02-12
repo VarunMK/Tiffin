0fa178cf10cebec6a89230ea3cd100889c8558c1
Kunal Bhat <kunal.bhat2001@gmail.com>
2022-02-12 16:00:57 +0530

    Fixed code-server issues

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-node /src/tfnrcv/index.js
+exec ${OPENVSCODE_SERVER_ROOT}/bin/openvscode-server --host 0.0.0.0 --without-connection-token "${@}" -- &
+node /src/index.js
--- docker/tiffin_recv/index.js
+++ docker/tiffin_recv/index.js
@@ -5,6 +5,18 @@ app.get("/", (req,res) => {
     res.status(200).send("no");
 })
 
+app.get("/pyinstall/:ver", (req,res) => {
+    console.log(`Install Python: ${ver}`);
+})
+
+app.get("/pip", (req,res)=>{
+    console.log(`Pip Install`);
+})
+
+app.get("/req", (req,res) => {
+    console.log("Installing reqs.")
+})
+
 app.listen(8080, ()=>{
     console.log("Started on 8080");
 })
