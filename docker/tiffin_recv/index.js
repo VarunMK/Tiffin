93cc7591404a5a1ac118bb0e5042de145646a4b6
Kunal Bhat <kunal.bhat2001@gmail.com>
2022-02-12 18:55:01 +0530

    Pyinstall

--- docker/tiffin_recv/index.js
+++ docker/tiffin_recv/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const child = require('child_process')
 const app = express()
 
 app.get("/", (req,res) => {
@@ -6,11 +7,25 @@ app.get("/", (req,res) => {
 })
 
 app.get("/pyinstall/:ver", (req,res) => {
-    console.log(`Install Python: ${ver}`);
+    console.log(`Install Python: ${req.params.ver}`);
+    let apt = child.exec(`sudo apt-get install python${req.params.ver} -y`);
+    apt.stdout.on('data', (data)=>{
+        console.log(`APT: ${data}`);
+    });
+    let ln = child.exec(`sudo ln -sf /usr/bin/python${req.params.ver} /usr/bin/python`);
+    ln.stdout.on("data", (data)=>{
+        console.log("LN completed");
+    })
+    res.status(200).send();
+    /*
+    console.log("Linked command");
+    res.status("200").send();
+    */
 })
 
 app.get("/pip", (req,res)=>{
     console.log(`Pip Install`);
+
 })
 
 app.get("/req", (req,res) => {
