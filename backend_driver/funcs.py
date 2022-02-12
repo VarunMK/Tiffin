1f4c646020988c686984476272d9c28e70c6aeb6
Utkarsh Gupta <utkarsh348@gmail.com>
2022-02-12 19:59:08 +0530

    index and create support

--- backend_driver/funcs.py
+++ backend_driver/funcs.py
@@ -2,14 +2,21 @@ from pydoc import doc
 from time import sleep
 import docker
 import requests
-
+import json
 client = docker.from_env()
 def start(container):
     container = client.containers.start(container)
 def create_cont(reqs,pyver):
-    container = client.containers.run('panzerox123/tiffin_contain', detach = False, ports = {'3000/tcp':3000, '8080/tcp':8080})
-    URL = "localhost:8080/pyinstall/"+pyver
+    container = client.containers.run('panzerox123/tiffin_contain', detach = True, ports = {'3000/tcp':3000, '8080/tcp':8080})
+    sleep(10)
+    URL = "http://localhost:8080/pyinstall/"+pyver
     r = requests.get(url = URL)
+    new = {container.short_id: reqs}
+    with open("index.json", "r+") as file:
+        data = json.load(file)
+        data.update(new)
+        file.seek(0)
+        json.dump(data, file)
 def run(volume):
     #need to run from volume
     pass
@@ -17,4 +24,3 @@ def stop(container):
     container = client.containers.stop(container)
 def destroy(container):
     pass
-
